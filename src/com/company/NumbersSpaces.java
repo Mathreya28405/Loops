059f35387b6139bae902a50d92ba80fa751d9f0f
mahathi <mahathiathreya@billericak12.com>
2019-12-10 21:31:56 -0500

    commit! <3

--- src/com/company/NumbersSpaces.java
+++ src/com/company/NumbersSpaces.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class NumbersSpaces {
+    public static void main (String [] args) {
+        //numSpace1();
+    }
+    public static int numSpace1() {
+        for (int i=1; i<=5; i++) {
+            for (int m=5-i; m>0; m--) {
+                System.out.print(" ");
+            }
+
+            for (int j=1; j<=i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        return 0;
+    }
+/*
+    1
+   12
+  123
+ 1234
+12345
+ */
+
+
+
 }
+
