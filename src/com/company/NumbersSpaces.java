fcca75a684438f69f46cba4a51cf81efd354c075
mahathi <mahathiathreya@billericak12.com>
2019-12-10 21:51:09 -0500

    commit! <3

--- src/com/company/NumbersSpaces.java
+++ src/com/company/NumbersSpaces.java
@@ -3,6 +3,8 @@ package com.company;
 public class NumbersSpaces {
     public static void main (String [] args) {
         //numSpace1();
+        //stars1();
+        stars2();
     }
     public static int numSpace1() {
         for (int i=1; i<=5; i++) {
@@ -24,6 +26,55 @@ public class NumbersSpaces {
  1234
 12345
  */
+    public static void stars1() {
+        for (int i = 0; i < 3; i++) {
+            for (int k=0; k<(i+1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int m=0; m<2; m++) {
+            for (int n=(2-m); n>0; n--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    /*
+     *
+     **
+     ***
+     **
+     *
+     */
+
+    public static void stars2() {
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<=i; j++) {
+                System.out.print("*");
+            }
+            for (int k=5-(2*i); k>0; k--) {
+                System.out.print(" ");
+            }
+            for (int j=0; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int q=0; q<7; q++) {
+            System.out.print("*");
+        }
+    }
+    /*
+
+     *     *
+     **   **
+     *** ***
+     *******
+
+     */
 
 
 
